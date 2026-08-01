e31f91409b6de40be5c63f2cfc9882f46f49ad35
awaisqarni-8 <awaisqarnismaq4@gmail.com>
2026-08-01 18:15:26 +0500

    Create product-details.js

---
+++ product-details.js
@@ -0,0 +1,64 @@
+import { db } from "./firebase.js";
+
+import {
+doc,
+getDoc
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+const params = new URLSearchParams(window.location.search);
+
+const id = params.get("id");
+
+const container = document.getElementById("productContainer");
+
+async function loadProduct() {
+
+const snap = await getDoc(doc(db, "products", id));
+
+if (!snap.exists()) {
+
+container.innerHTML = "<h2>Product Not Found</h2>";
+
+return;
+
+}
+
+const product = snap.data();
+
+container.innerHTML = `
+
+<div class="productDetails">
+
+<img src="${product.image}" alt="${product.name}">
+
+<div class="info">
+
+<h1>${product.name}</h1>
+
+<h2>PKR ${product.price}</h2>
+
+<p>${product.description}</p>
+
+<p><b>Category:</b> ${product.category}</p>
+
+<p><b>Stock:</b> ${product.stock}</p>
+
+<br>
+
+<button class="cartBtn">
+Add to Cart
+</button>
+
+<button class="buyBtn">
+Buy Now
+</button>
+
+</div>
+
+</div>
+
+`;
+
+}
+
+loadProduct();
