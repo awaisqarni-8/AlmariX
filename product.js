75dc2194847969a5568b948f9ab59b75fd93db90
awaisqarni-8 <awaisqarnismaq4@gmail.com>
2026-08-02 18:15:37 +0500

    Update product.js

--- product.js
+++ product.js
@@ -35,7 +35,25 @@ productGrid.innerHTML += `
 <div class="productCard"
 onclick="window.location.href='product.html?id=${item.id}'">
 
-<img src="${product.image}" alt="${product.name}">
+<div class="productImages">
+
+<img id="mainImage" src="${product.image}" alt="${product.name}" width="350">
+
+<div class="thumbs">
+
+<img src="${product.image}"
+width="80"
+onclick="document.getElementById('mainImage').src='${product.image}'">
+
+${product.image2 ? `
+<img src="${product.image2}"
+width="80"
+onclick="document.getElementById('mainImage').src='${product.image2}'">
+` : ""}
+
+</div>
+
+</div>
 
 <h3>${product.name}</h3>
 
